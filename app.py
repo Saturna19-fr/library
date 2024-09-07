f9e70ea1ec4836362944e50d665a7f44dca2f4f0
Saturna19 <contact@saturna19.fr>
2024-09-07 15:47:42 +0200

    Changed logic to hide secrets

--- app.py
+++ app.py
@@ -3,42 +3,82 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 from auth.auth import auth_bp
 from dotenv import load_dotenv
-
+import requests
+from sqlalchemy.exc import IntegrityError
 load_dotenv()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_CONNECTION_STRING")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["environnement"] = environ.get("TYPE_OF_ENV") or "PRODUCTION"
 
 app.register_blueprint(auth_bp)
 
 db = SQLAlchemy(app)
 
+def getDataByISBN(isbn)->dict:
+    d = requests.get(url=f"https://api2.isbndb.com/book/{isbn}", headers={"Authorization": environ.get("ISBN_DB_API_KEY")})
+    if d.status_code != 200:
+        return {"error": 404, "message": d.json()["errorMessage"]}
+    return d.json()
+
+        
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     isbn = db.Column(db.String(80), unique=True, nullable=False)
     count = db.Column(db.Integer, unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
 @app.route("/")
 def index():
-    return render_template("index.html", apikey = environ.get("ISBN_DB_API_KEY"), baseurl = environ.get("ISBN_DB_ENDPOINT"))
-
+    return render_template("index.html")
 
 @app.route("/api/addbook", methods=["POST"])
 def addbook():
+    bookData = None
     try:
-
-        headers = request.headers
-        book = Book(name=headers.get('Title'), isbn=headers.get('ISBN'), count=1)
+        bookData = getDataByISBN(request.headers.get('ISBN'))
+        if "error" in bookData:
+            return jsonify({"status": 404, "message": bookData["message"]})
+    except Exception as e:
+        return jsonify({"status": 500, "message": f"An error occured !\n {e}"})
+        #END
+    else:
+        #Continue
+        print("Données récupérées.")
+    
+    try:
+        book = Book(name = bookData["book"]["title"], isbn = request.headers.get("ISBN"), count = 1)
         db.session.add(book)
         db.session.commit()
-        return jsonify({"status": 200, "message": "Book added successfully", "id": book.id})
+        return jsonify({"status": 201, "message": "Book added successfully", "id": book.id})
+    except IntegrityError as e:
+        return jsonify({"status": 409, "message": "Book already exists"})
+        #END
     except Exception as e:
         return jsonify({"status": 500, "message": f"Internal Server Error {e}"})
+        #END
+        
+
+# @app.route("/api/addbook", methods=["POST"])
+# def addbook():
+#     bookData = None
+#     try:
+#         bookData = getDataByISBN(request.headers.get('ISBN'))
+#     except Exception as e:
+#         return jsonify({"status": 500, "message": f"An error occured !\n {e}"})
+#     try:
+#         headers = request.headers
+#         book = Book(name=headers.get('Title'), isbn=headers.get('ISBN'), count=1)
+#         db.session.add(book)
+#         db.session.commit()
+#         return jsonify({"status": 200, "message": "Book added successfully", "id": book.id})
+#     except Exception as e:
+#         return jsonify({"status": 500, "message": f"Internal Server Error {e}"})
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=443, debug=True, load_dotenv=True, use_reloader=True, ssl_context = ('.local/cert1.pem', '.local/privkey1.pem'))
-    app.run(host="0.0.0.0", port=5000, load_dotenv=True)
+    if app.config["environnement"] == "DEV":
+        app.run(host="0.0.0.0", port=443, debug=True, load_dotenv=True, use_reloader=True, ssl_context = ('.local/cert1.pem', '.local/privkey1.pem'))
+    elif app.config["environnement"] == "PRODUCTION":
+        app.run(host="0.0.0.0", port=5000, load_dotenv=True)
+    else:
+        print("Une erreur s'est produite", "\nVous devez spécifier un environnement de travail")
--- static/js/reader.js
+++ static/js/reader.js
@@ -29,70 +29,105 @@ window.addEventListener('load', function () {
                 searchItemByISBN(isbn.value, true);
                 isbn.value = "";
             }
+
             function searchItemByISBN(ISBN, isManual = false) {
-                
                 if (debounce) return false;
                 debounce = true;
                 codeReader.reset()
-                fetch(window.baseurl + '/book/' + ISBN, { headers: myHeaders })
-                    //db281ba3800a48399ac5ba7de01f8763
-                    .then(response => response.json())
-                    .then(dataapi => {
-                        if (dataapi.errorMessage) {
-                            resultparag.innerHTML = "Code-barres non reconnu err end isbn";
-                            alert("Erreur ISBN (refaire le scan, sinon mettre de coté)")
-                            return startCamera();
-                        }
-                        console.log(dataapi);
-                        if (dataapi.error) {
-                            resultparag.innerHTML = "Code-barres non reconnu";
-                        } else {
-                            if (isManual){
-                                alert("Found !"+ dataapi.book.title_long)
-                            }
-                            resultparag.innerHTML = dataapi.book.title_long + " - " + dataapi.book.publisher;
-                            addItem(dataapi.book.title_long, ISBN);
-                            setTimeout(function () {
-                                startCamera();
-                            }, 2000);
-                        }
-                    })
-                    .catch((r) => {
-                        resultparag.innerHTML = "Erreur ! " + r;
-                        setTimeout(function () {
-                            startCamera();
-                        }, 2000);
-            
-                    })
-            }
-            
-            function addItem(TITLE, ISBN) {
-                fetch(`/api/addbook`, {
+                fetch("/api/addbook", {
                     method: 'POST',
                     headers: {
-                        'Title': TITLE,
                         'ISBN': ISBN,
                     }
                 })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log(data);
-                        if (data.code == 400) {
-                            resultparag.innerHTML = "Erreur flask !";
-                            alert("Error flask !");
-                            resultparag.innerHTML = `<strong>Erreur ! ${data.message}</strong>`;
-                        } else {
-                            resultparag.innerHTML = "Ajouté !";
-                            alert("Added ?")
-            
-                        }
-                        setTimeout(function () {
-                            startCamera();
-                        }, 2000);
-                    })
+                .then(response => response.json())
+                .then(data => {
+                    
+                    if (data.status == 201) {
+                        resultparag.innerHTML = "Livre ajouté correctement !";
+                        alert("Le livre a été ajouté correctement !");
+                        return startCamera();
+                    } else if (data.status == 409) {
+                        resultparag.innerHTML = "Le livre est déjà présent dans la base de données !";
+                        alert("Le livre est déjà présent dans la base de données !");
+                        return startCamera();
+                    } else if (data.status == 500) {
+                        resultparag.innerHTML = "Erreur interne !";
+                        alert("Erreur interne !");
+                        return startCamera();
+                    } else if (data.status == 404) {
+                        resultparag.innerHTML = "ISBN non trouvé !";
+                        alert("ISBN non trouvé !");
+                        return startCamera();
+                    }
+                })
+
             }
 
-            var alreadyScanned = [];
+            // function searchItemByISBN(ISBN, isManual = false) {
+                
+            //     if (debounce) return false;
+            //     debounce = true;
+            //     codeReader.reset()
+            //     fetch(window.baseurl + '/book/' + ISBN, { headers: myHeaders })
+            //         //db281ba3800a48399ac5ba7de01f8763
+            //         .then(response => response.json())
+            //         .then(dataapi => {
+            //             if (dataapi.errorMessage) {
+            //                 resultparag.innerHTML = "Code-barres non reconnu err end isbn";
+            //                 alert("Erreur ISBN (refaire le scan, sinon mettre de coté)")
+            //                 return startCamera();
+            //             }
+            //             console.log(dataapi);
+            //             if (dataapi.error) {
+            //                 resultparag.innerHTML = "Code-barres non reconnu";
+            //             } else {
+            //                 if (isManual){
+            //                     alert("Found !"+ dataapi.book.title_long)
+            //                 }
+            //                 resultparag.innerHTML = dataapi.book.title_long + " - " + dataapi.book.publisher;
+            //                 addItem(dataapi.book.title_long, ISBN);
+            //                 setTimeout(function () {
+            //                     startCamera();
+            //                 }, 2000);
+            //             }
+            //         })
+            //         .catch((r) => {
+            //             resultparag.innerHTML = "Erreur ! " + r;
+            //             setTimeout(function () {
+            //                 startCamera();
+            //             }, 2000);
+            
+            //         })
+            // }
+            
+            // function addItem(ISBN) {
+            //     fetch(`/api/addbook`, {
+            //         method: 'POST',
+            //         headers: {
+            //             'Title': TITLE,
+            //             'ISBN': ISBN,
+            //         }
+            //     })
+            //         .then(response => response.json())
+            //         .then(data => {
+            //             console.log(data);
+            //             if (data.code == 400) {
+            //                 resultparag.innerHTML = "Erreur flask !";
+            //                 alert("Error flask !");
+            //                 resultparag.innerHTML = `<strong>Erreur ! ${data.message}</strong>`;
+            //             } else {
+            //                 resultparag.innerHTML = "Ajouté !";
+            //                 alert("Added ?")
+            
+            //             }
+            //             setTimeout(function () {
+            //                 startCamera();
+            //             }, 2000);
+            //         })
+            // }
+
+            // var alreadyScanned = [];
             document.getElementById('resetButton').addEventListener('click', () => {
                 codeReader.reset()
                 resultparag.textContent = '';
@@ -117,7 +152,7 @@ window.addEventListener('load', function () {
             }
             setTimeout(function () {
                 startCamera();
-            }, 2000);
+            }, 1000);
 
             document.getElementById('startButton').addEventListener('click', () => startCamera())
         })
